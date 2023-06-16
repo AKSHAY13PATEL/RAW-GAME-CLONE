7ee76563c44ab802b566ffcc7750b72eb6f97489
Akshay Patel <akshaypatel.ap1320@gmail.com>
2023-06-16 18:32:06 +0530

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,6 @@
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return "";
+
   const index = url.indexOf("media/");
   const croppedImageUrl =
     url.slice(0, index + 6) + "crop/600/400/" + url.slice(index + 6);
