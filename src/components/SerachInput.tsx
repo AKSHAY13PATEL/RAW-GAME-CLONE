4d32da1e5f891b8ba358562679fe933f5116828c
Akshay Patel <akshaypatel.ap1320@gmail.com>
2023-06-17 14:56:12 +0530

    Building search game

--- src/components/SerachInput.tsx
+++ src/components/SerachInput.tsx
@@ -1,16 +1,31 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SerachInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SerachInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        borderRadius={30}
-        placeholder="Search the game..."
-        variant="filled"
-      ></Input>
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={30}
+          placeholder="Search the game..."
+          variant="filled"
+        ></Input>
+      </InputGroup>
+    </form>
   );
 };
 
