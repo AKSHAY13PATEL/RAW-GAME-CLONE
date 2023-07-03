11774f4ff9c9d9085a33b114c79137d72ec7dd7c
Akshay Patel <akshaypatel.ap1320@gmail.com>
2023-07-03 20:37:59 +0530

    Modification to game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,24 +13,21 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletonNumber = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-
-      <SimpleGrid spacing={3} columns={{ sm: 1, md: 3, lg: 4 }} padding="10px">
-        {isLoading &&
-          skeletonNumber.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid spacing={3} columns={{ sm: 1, md: 3, lg: 4 }} padding="10px">
+      {isLoading &&
+        skeletonNumber.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
